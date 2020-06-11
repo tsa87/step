96bbd7f0b3052571c67a910801b2554cf6f7f6fb
Tony Shen <tsa87@sfu.ca>
2020-06-11 02:47:40 +0000

    fix string lower case bug

--- portfolio/src/main/java/com/google/sps/model/SentimentEvaluator.java
+++ portfolio/src/main/java/com/google/sps/model/SentimentEvaluator.java
@@ -3,17 +3,19 @@ package com.google.sps.model;
 import com.google.cloud.language.v1.Document;
 import com.google.cloud.language.v1.LanguageServiceClient;
 import com.google.cloud.language.v1.Sentiment;
+import java.io.IOException;
 
 public class SentimentEvaluator {
 
-	public static float getSentiment(string message) {
+	public static float getSentiment(String message) throws IOException {
 		Document doc =
-        Document.newBuilder().setContent(message).setType(Document.Type.PLAIN_TEXT).build();
-		LanguageServiceClient languageService = LanguageServiceClient.create();
-    Sentiment sentiment = languageService.analyzeSentiment(doc).getDocumentSentiment();
-    float score = sentiment.getScore();
-    languageService.close();
+        	Document.newBuilder().setContent(message).setType(Document.Type.PLAIN_TEXT).build();
 
+        LanguageServiceClient languageService = LanguageServiceClient.create();
+        Sentiment sentiment = languageService.analyzeSentiment(doc).getDocumentSentiment();
+        float score = sentiment.getScore();
+        languageService.close();
+        
 		return score;
 	}
 
